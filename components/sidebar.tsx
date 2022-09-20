287ed855f0c4e0127274730d930022ee892b5d90
Altamash Ali <14983914+Altamashattari@users.noreply.github.com>
2022-09-20 17:55:55 +0530

    fetching data

--- components/sidebar.tsx
+++ components/sidebar.tsx
@@ -20,6 +20,7 @@ import {
   MdFavorite,
 } from "react-icons/md";
 import NextLink from "next/link";
+import { usePlaylist } from "../lib/hooks";
 
 const navMenu = [
   {
@@ -52,9 +53,8 @@ const musicMenu = [
   },
 ];
 
-const playlists = new Array(30).fill(1).map((_, i) => `Playlist ${i + 1}`);
-
 const Sidebar = () => {
+  const { playlists, isLoading, isError } = usePlaylist();
   return (
     <Box
       width="100%"
@@ -110,11 +110,13 @@ const Sidebar = () => {
         <Divider color="gray.800" />
         <Box height="66%" overflowY="auto" paddingY="20px">
           <List spacing={2}>
+            {isLoading && <div>Loading Playlists...</div>}
+            {isError && <div>Error while fetching playlists..</div>}
             {playlists.map((playlist) => (
-              <ListItem paddingX="20px" key={playlist}>
+              <ListItem paddingX="20px" key={playlist.id}>
                 <LinkBox>
                   <NextLink href="/" passHref>
-                    <LinkOverlay>{playlist}</LinkOverlay>
+                    <LinkOverlay>{playlist.name}</LinkOverlay>
                   </NextLink>
                 </LinkBox>
               </ListItem>
