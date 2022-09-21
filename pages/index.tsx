b07e95a0a9e73a264703cffcd4d00c3754070b6b
Altamash Ali <14983914+Altamashattari@users.noreply.github.com>
2022-09-21 08:01:57 +0530

    gradient layout

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,19 @@
 import React from "react";
+import GradientLayout from "../components/gradientLayout";
 
 const Home = () => {
-  return <div>Home</div>;
+  return (
+    <GradientLayout
+      color="green"
+      subtitle="profile"
+      title="Altamash Ali"
+      description="15 public playlists"
+      image="https://images.pexels.com/photos/771742/pexels-photo-771742.jpeg"
+      roundImage
+    >
+      <div>Home Page</div>
+    </GradientLayout>
+  );
 };
 
 export default Home;
