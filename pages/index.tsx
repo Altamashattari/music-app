c13ef56e49c25deefc00a8d64196e72c11bb5e6c
Altamash Ali <14983914+Altamashattari@users.noreply.github.com>
2022-09-21 08:26:05 +0530

    home page

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,10 @@
+import { Box, Flex, Text } from "@chakra-ui/layout";
+import { Image } from "@chakra-ui/react";
 import React from "react";
 import GradientLayout from "../components/gradientLayout";
+import prisma from "../lib/prisma";
 
-const Home = () => {
+const Home = ({ artists = [] }) => {
   return (
     <GradientLayout
       color="green"
@@ -11,9 +14,42 @@ const Home = () => {
       image="https://images.pexels.com/photos/771742/pexels-photo-771742.jpeg"
       roundImage
     >
-      <div>Home Page</div>
+      <Box color="white" paddingX="40px">
+        <Box marginBottom={10}>
+          <Text fontSize="2xl" fontWeight="bold">
+            Top artists of the Month
+          </Text>
+          <Text fontSize="medium">only visible to you</Text>
+        </Box>
+        <Flex>
+          {artists.map((artist) => (
+            <Box paddingX="10px" width="20%">
+              <Box bg="gray.900" borderRadius="4px" padding="15px" width="100%">
+                <Image
+                  src="https://images.unsplash.com/photo-1517230878791-4d28214057c2?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1738&q=80"
+                  borderRadius="100%"
+                />
+                <Box marginTop="20px">
+                  <Text fontSize="large">{artist.name}</Text>
+                  <Text fontSize="smaller">Artist</Text>
+                </Box>
+              </Box>
+            </Box>
+          ))}
+        </Flex>
+      </Box>
     </GradientLayout>
   );
+};
+
+export const getServerSideProps = async () => {
+  const artists = await prisma.artist.findMany({});
+
+  return {
+    props: {
+      artists,
+    },
+  };
 };
 
 export default Home;
